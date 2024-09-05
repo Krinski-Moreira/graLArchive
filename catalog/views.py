d89046ba3afad01dbc3181e2230489d9d93b1389
unknown <otavioskm@gmail.com>
2024-09-04 23:48:10 -0300

    templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Lens, LensComponent
 
 # Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+    num_lenses = Lens.objects.all().count()
+    num_gaia = Lens.objects.filter(GraL="TRUE").count()
+
+    context = {
+        'num_lenses' : num_lenses,
+        'num_gaia' : num_gaia,
+    }
+
+    return render(request, 'index.html', context=context)
--- graLArchive/urls.py
+++ graLArchive/urls.py
@@ -33,3 +33,9 @@ from django.views.generic import RedirectView
 urlpatterns += [
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
+
+# Use static() to add URL mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
