38ef6f1e62f9a7c1313e234e3b774337e12445ea
unknown <otavioskm@gmail.com>
2024-09-16 10:06:09 -0300

    checkboxes and download button

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lens/', views.lens, name='lens')
+    path('lens/', views.lens, name='lens'),
+    path('export_csv', views.export_csv, name='export_csv')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Lens, LensComponent
 from django.views import generic
+from .forms import CreatefieldsForm
+import csv
+from django.http import HttpResponse
+
 
 # Create your views here.
 
@@ -43,26 +47,39 @@ def lens(request):
     compfields = [f.name for f in LensComponent._meta.get_fields()]
     #print(compfields, lensfields)
     fields = lensfields[2:] + compfields[2:]
-    '''table = [fields]
+    savedfields = request.session.get('sfields', fields)
+    form = CreatefieldsForm()
+    options = []
+    for i in range(len(fields)):
+        options.append((i,fields[i]))
 
-    for component in components:
-        line = []
-        lens = Lens.objects.filter(id = component['Name_id']).values()
-        #print(component, lens, type(lens))
-        for k, v in lens[0].items():
-            #print(k, v)
-            if(k in fields):
-                line.append(v)
-        for k, v in component.items():
-            #print(k, v)
-            if(k in fields):
-                line.append(v)
-        table.append(line)'''
-    table = create_table(fields)
+    form_values = request.GET.getlist('fieldsform')
+    request.session['sfields'] = form_values
+    savedfields = form_values
+    print(form_values)
 
-    #print(table)
+    table = create_table(form_values)
+
     context = {
         'table' : table,
+        "form": form,
+        "options": options,
+        "savedfields": savedfields
     }
 
     return render(request, 'catalog/lens_list.html', context=context)
+
+def export_csv(request):
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(
+        content_type="text/csv",
+        headers={"Content-Disposition": 'attachment; filename="database.csv"'},
+    )
+
+    writer = csv.writer(response)
+    savedfields = request.session.get('sfields')
+    table = create_table(savedfields)
+    for row in table:
+        writer.writerow(row)
+
+    return response
